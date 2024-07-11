924916424533db2c9327b0c2b167d0b77a716c14
Fabrice David <fabrice.david@epfl.ch>
2024-07-11 12:02:34 +0200

    secure cookie

--- src/config/initializers/session_store.rb
+++ src/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 #Rails.application.config.after_initialize do
-  Rails.application.config.session_store :active_record_store, :key => '_dgrpool_session'
+  Rails.application.config.session_store :active_record_store, :key => '_dgrpool_session', :secure => true
 #end
